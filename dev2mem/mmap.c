c1b10185c0b4bad64c9568af63e441e7944d7fbf
timteng <linyangspring@aliyun.com>
2014-07-08 17:58:28 +0800

    Signed-off-by: timteng <linyangspring@aliyun.com> 07/07/2014 更新了qspiflash 初始烧写文档

--- dev2mem/mmap.c
+++ dev2mem/mmap.c
@@ -28,28 +28,45 @@
 #include <sys/mman.h>
 #include <string.h>
 
+#define PAGE_SIZE ((size_t)getpagesize())
+#define PAGE_MASK ((uint64_t)(long)~(PAGE_SIZE - 1))
+
 int main(int argc, char **argv)
 {
-    int mmcfd;
-    char string[40];
-    int len, res;
+    int fd;
+    uint64_t offset, base;
+    volatile uint8_t *mm;
+    int i;
 
-    mmcfd = open("/mnt/testrecord", O_RDWR | O_CREAT | O_SYNC);
-    if(mmcfd < 0) {
-    	fprintf(stderr, "open (/mnt/testrecord) failed (%d) \n", errno);
+    fd = open("/dev/mem", O_RDWR | O_SYNC);
+    if(fd < 0) {
+    	fprintf(stderr, "open (/dev/mem) failed (%d) \n", errno);
     	return 1;
     }
 
-    strcpy(string, "data fresh for amp_intr\n");
-    len = strlen(string);
+    offset = 0x3FF00000;
+    base = offset & PAGE_MASK;
+    offset &= ~PAGE_MASK;
+    fprintf(stdout, "PAGE_SIZE (0x%x) \n", PAGE_SIZE);
+    fprintf(stdout, "PAGE_MASK (0x%x) \n", PAGE_MASK);
+    fprintf(stdout, "base (0x%x) \n", base);
+    fprintf(stdout, "offset (0x%x) \n", offset);
 
-    res = lseek(mmcfd, 0, SEEK_END);//在尾部追加数据
-    if((res = write(mmcfd, string, len)) != len) {
-    	fprintf(stderr, "error writing mmc\n");
-    	return 1;
+    mm = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE,
+    		MAP_SHARED, fd, base);
+    if(MAP_FAILED == mm) {
+    	fprintf(stderr, "mmap64 (0x%x@0x%x) failed (%d) \n",
+    			PAGE_SIZE, base, errno);
     }
-	fsync(mmcfd);//将数据刷新到磁盘硬件
-    close(mmcfd);
+    else {
+    	fprintf(stdout, "mmap64 (0x%x) success!\n", base);
+    }
+    for(i=0; i<576; i++) {
+    	*(volatile uint32_t *)(mm + offset + (i<<2)) = 0xffffffff;
+    }
+
+    munmap((void *)mm, PAGE_SIZE);
+    close(fd);
 
     return 0;
 }
